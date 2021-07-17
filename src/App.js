b286f2a1081c62623006d4fafad284ac562e877a
Diego <drbv27@gmail.com>
2021-07-17 07:18:31 -0500

    Practice with JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let nombre="Diego";
+  let auth = false;
+  let estaciones = ["Primavera","Verano","Otoño","Invierno"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre"/>
+        <h1>{nombre}</h1>
+        <p>
+          {auth ? "El usuario está autenticado":"El usuario no es valido"}
+        </p>
+        <p>{2+1}</p>
         <p>
           Editar <code>src/App.js</code> and save to reload.
         </p>
+        <ul>{estaciones.map((el,index) => (
+          <li key={index}>{el}</li>
+        ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
