875bd4533e41f334a7647d7e2d4ad77a9686882a
Diego <drbv27@gmail.com>
2021-07-18 07:19:44 -0500

    Testing props use

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import Componente from './components/Componente';
+import Propiedades from './components/Propiedades'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Editar <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg = "Hola soy un Componente Funcional Expresado desde una prop"/>
+          <hr/>
+          <Propiedades
+          cadena="Esto es una cadena de texto"
+          numero={19}
+          booleano={true}
+          arreglo = {[1,2,3]}
+          objeto = {{nombre:"Diego",correo:"drbv27@gmail.com"}}
+          />
+        </section>
       </header>
-      <section>
-        <Componente msg = "Hola soy un Componente Funcional Expresado desde una prop"/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      </section>
     </div>
   );
 }
