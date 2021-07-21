cccaf6a10b8c9c8f6c9e980ad8e809988875d95f
Diego <drbv27@gmail.com>
2021-07-21 18:01:38 -0500

    Conditional render

--- src/components/Estado.js
+++ src/components/Estado.js
@@ -14,11 +14,11 @@ export default class Estado extends Component {
         this.state = {
             contador:0,
         };
-        setInterval(() => {
+/*         setInterval(() => {
             this.setState({
                 contador:this.state.contador+1
             })
-        }, 1000);
+        }, 1000); */
     }
     render(){
        return(
