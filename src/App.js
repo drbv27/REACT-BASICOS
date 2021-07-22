13c9ce463a001b1648d0fd10729ccf4f671d4325
Diego <drbv27@gmail.com>
2021-07-22 10:30:38 -0500

    Triying events

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Propiedades from './components/Propiedades'
 import Estado from './components/Estado'
 import RenderizadoCondicional from './components/RenderizadoCondicional'
 import RenderizadoElementos from './components/RenderizadoElementos';
+import Eventos from './components/Eventos';
 import './App.css';
 
 function App() {
@@ -46,6 +47,8 @@ function App() {
           <RenderizadoCondicional/>
           <hr/>
           <RenderizadoElementos/>
+          <hr/>
+          <Eventos/>
         </section>
       </header>
     </div>
