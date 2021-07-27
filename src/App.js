69b35b1af8a589bc1c8412f3ce394de3ddab1872
Diego <drbv27@gmail.com>
2021-07-26 21:54:39 -0500

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import RenderizadoCondicional from './components/RenderizadoCondicional'
 import {EventosES6,EventosES7,MasSobreEventos} from './components/Eventos';
 import RenderizadoElementos from './components/RenderizadoElementos';
 import ComunicacionComponentes from './components/ComunicacionComponenentes';
+import CicloVida  from './components/CicloVida';
 import './App.css';
 
 
@@ -57,6 +58,8 @@ function App() {
           <MasSobreEventos/>
           <hr/>
           <ComunicacionComponentes/>
+          <hr/>
+          <CicloVida />
         </section>
       </header>
     </div>
